6abba5a231bf5de1cd144d7b00b592871529efc5
Adam <845679+adamarbour@users.noreply.github.com>
2026-08-02 22:32:41 -0500

    Basic yubikey

--- nix/nixos/yubikey.nix
+++ nix/nixos/yubikey.nix
@@ -1,6 +1,6 @@
 {
     flake.modules.nixos.yubikey = { pkgs, ... }: {
-        services.pcsdc.enable = true;
+        services.pcscd.enable = true;
         services.udev.packages = [ pkgs.yubikey-personalization ];
         environment.systemPackages = with pkgs; [
             yubikey-manager
