5cf4f07e8c6be63fa985415d585d9e223f42585c
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-08-08 13:54:20 -0500

    Removal of some hardware code migrated to config

--- nix/machines/cirilla/hardware.nix
+++ nix/machines/cirilla/hardware.nix
@@ -1,20 +1,12 @@
 {
     flake.modules.nixos.cirilla = { lib, modulesPath, ... }: {
-        imports = [
-            (modulesPath + "/installer/scan/not-detected.nix")
-        ];
+        imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
         boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
         boot.initrd.kernelModules = [ ];
         boot.kernelModules = [ "kvm-amd" "amdgpu" ];
-        boot.extraModulePackages = [ ];
 
         services.xserver.videoDrivers = [ "amdgpu" ];
-
-        hardware.graphics = {
-            enable = true;
-            enable32Bit = true; # Steam/Proton
-        };
 
         hardware.cpu.amd.updateMicrocode = lib.mkDefault true;
     };
