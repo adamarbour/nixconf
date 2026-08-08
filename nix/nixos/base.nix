de1218c6b685e508ec11b7741325767e9e669201
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-08-08 13:53:46 -0500

    Better baseline

--- nix/nixos/base.nix
+++ nix/nixos/base.nix
@@ -1,11 +1,12 @@
 { inputs, self, ... }:
 {
-    flake.modules.nixos.base = { pkgs, lib, ... }: {
+    flake.modules.nixos.base = { pkgs, lib, config, ... }: {
 
         imports = [
             # inputs
             inputs.hjem.nixosModules.default
             # self
+            self.modules.nixos.kernel-blacklist
             self.modules.nixos.user-adam
         ];
 
@@ -35,6 +36,8 @@
             LC_TELEPHONE = "en_US.UTF-8";
             LC_TIME = "en_US.UTF-8";
         };
+
+        # nix settings
 
         # https://github.com/NixOS/nixpkgs/blob/eca4605163a534aed1981de0f5f1d7d7639d1640/nixos/modules/programs/environment.nix#L18
         environment.variables.NIXPKGS_CONFIG = lib.mkForce "";
@@ -112,6 +115,41 @@
                 };
             })
         ];
+
+        # security
+        environment.etc = {
+            # Empty /etc/securetty to prevent root login on tty.
+            securetty.text = ''
+            # /etc/securetty: list of terminals on which root is allowed to login.
+            # See securetty(5) and login(1).
+            '';
+
+            # Set machine-id to the Kicksecure machine-id, for privacy reasons.
+            # /var/lib/dbus/machine-id doesn't exist on dbus enabled NixOS systems,
+            # so we don't have to worry about that.
+            machine-id.text = ''
+            b08dfa6083e7567a1921a715000001fb
+            '';
+        };
+
+        # boot
+        boot = {
+            loader.timeout = lib.mkDefault 3;
+            loader.systemd-boot.editor = lib.mkDefault false;
+            consoleLogLevel = 3;
+            initrd.verbose = false;
+
+            kernelParams = [
+                "nowatchdog"
+                "nmi_watchdog=0"
+            ];
+
+            tmp = {
+                useTmpfs = lib.mkDefault true;
+                tmpfsHugeMemoryPages = lib.mkDefault "within_size";
+                cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
+            };
+        };
 
         system.stateVersion = "26.05";
 
--- nix/nixos/graphical.nix
+++ nix/nixos/graphical.nix
@@ -1,6 +1,8 @@
 { inputs, self, ... }:
 {
-    flake.modules.nixos.graphical = { pkgs, lib, ... }: {
+    flake.modules.nixos.graphical = { pkgs, lib, ... }: let
+        selfpkgs = self.packages."${pkgs.stdenv.hostPlatform.system}";
+    in {
         # locale
         services.xserver.xkb = {
             layout = "us";
@@ -13,12 +15,52 @@
             neovide
             # development
             github-cli
+            # selfpkgs
+            selfpkgs.nh
         ];
 
         # hardware assumption
         hardware.graphics = {
             enable = true;
             enable32Bit = true;
+        };
+
+        # boot
+        boot = {
+            # increase the map count, this is important for applications that require a lot of memory mappings
+            # such as games and emulators
+            kernel.sysctl."vm.max_map_count" = 2147483642;
+
+            # assume silent boot
+            plymouth = {
+                enable = true;
+                theme = "bgrt";
+            };
+            kernelParams = [
+                # silent
+                "quiet"
+                "loglevel=3"
+                "udev.log_level=3"
+                "rd.udev.log_level=3"
+                "systemd.show_status=auto"
+                "rd.systemd.show_status=auto"
+
+                # https://en.wikipedia.org/wiki/Kernel_page-table_isolation
+                # auto means kernel will automatically decide the pti state
+                "pti=auto" # on || off
+
+                # enable IOMMU for devices used in passthrough and provide better host performance
+                "iommu=pt"
+
+                # allow systemd to set and save the backlight state
+                "acpi_backlight=native"
+
+                # prevent the kernel from blanking plymouth out of the fb
+                "fbcon=nodefer"
+
+                # disable the cursor in vt to get a black screen during intermissions
+                "vt.global_cursor_default=0"
+            ];
         };
     };
 }
