2fcafb9f6a365664bbd6611c504fba3f6373921c
Adam <845679+adamarbour@users.noreply.github.com>
2026-08-09 00:12:47 -0500

    moving to kde across devices

--- nix/machines/hyphasis/configuration.nix
+++ nix/machines/hyphasis/configuration.nix
@@ -19,11 +19,12 @@
         networking.networkmanager.enable = true;
 
         # Enable the X11 windowing system.
+        # You can disable this if you're only using the Wayland session.
         services.xserver.enable = true;
 
-        # Enable the XFCE Desktop Environment.
-        services.xserver.displayManager.lightdm.enable = true;
-        services.xserver.desktopManager.xfce.enable = true;
+        # Enable the KDE Plasma Desktop Environment.
+        services.displayManager.sddm.enable = true;
+        services.desktopManager.plasma6.enable = true;
 
         # Enable CUPS to print documents.
         services.printing.enable = true;
