ade0af8a8b68d77e5288722baeb4a91084ac83ac
Adam <845679+adamarbour@users.noreply.github.com>
2026-08-14 20:32:01 -0500

    Fixing gamescope for device

--- nix/machines/hellespont/hardware.nix
+++ nix/machines/hellespont/hardware.nix
@@ -4,7 +4,7 @@
     imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
 
-    programs.gamemade.settings.gpu = {
+    programs.gamemode.settings.gpu = {
       gpu_device = lib.mkForce 0;
       amd_performance_level = lib.mkForce "high";
     };
