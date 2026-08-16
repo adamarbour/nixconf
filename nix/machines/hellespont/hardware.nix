a0f19749da3406a4f988995f2d404722c8e46b52
Adam <845679+adamarbour@users.noreply.github.com>
2026-08-16 13:22:42 -0500

    Fixing wifi

--- nix/machines/hellespont/hardware.nix
+++ nix/machines/hellespont/hardware.nix
@@ -4,6 +4,11 @@
     imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
 
+    # not working for some reason so we force to Wi-Fi 5
+    boot.extraModprobeConfig = ''
+      options iwlwifi disable_11ax=1
+    '';
+
     programs.gamemode.settings.gpu = {
       gpu_device = lib.mkForce 0;
       amd_performance_level = lib.mkForce "high";
