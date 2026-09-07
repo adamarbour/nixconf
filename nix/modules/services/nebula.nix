638ed75ac76727766dbfe2f3ee2c9bd9f7894c67
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-09-07 11:50:48 -0500

    Fixes for secrets handling

--- nix/modules/services/nebula.nix
+++ nix/modules/services/nebula.nix
@@ -2,6 +2,7 @@
   flake.modules.nixos.my-services =
     {
       lib,
+      pkgs,
       config,
       ...
     }:
@@ -11,14 +12,30 @@
     {
       options.my.services.nebula = {
         enable = lib.mkEnableOption "enable nebula service";
+        canSign = lib.mkEnableOption "enable nebula CA sign";
       };
 
       config = lib.mkIf cfg.nebula.enable {
         sops.secrets = lib.mkIf config.nixSecrets.enable {
           "nebula-ca-crt" = {
             key = "nebula/ca-crt";
+            mode = "0444";
+          };
+          "nebula-ca-key" = lib.mkIf cfg.nebula.canSign {
+            key = "nebula/ca-key";
+            owner = "adam";
+            mode = "0400";
           };
         };
+
+        environment.systemPackages = lib.mkIf cfg.nebula.canSign [
+          (pkgs.writeShellScriptBin "nebula-sign-host" ''
+            nebula-cert sign \
+              -ca-crt ${config.sops.secrets."nebula-ca-crt".path} \
+              -ca-key ${config.sops.secrets."nebula-ca-key".path} \
+              "$@"
+          '')
+        ];
       };
     };
 }
