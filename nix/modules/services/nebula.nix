85fb846562ff3aa5233ff441a9f2acdcbc80adfe
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-09-07 12:20:33 -0500

    Expanded nebula for initial trial

--- nix/modules/services/nebula.nix
+++ nix/modules/services/nebula.nix
@@ -13,6 +13,11 @@
       options.my.services.nebula = {
         enable = lib.mkEnableOption "enable nebula service";
         canSign = lib.mkEnableOption "enable nebula CA sign";
+        port = lib.mkOption {
+          type = lib.types.port;
+          default = 4242;
+          description = "UDP port this nebula node listens on.";
+        };
       };
 
       config = lib.mkIf cfg.nebula.enable {
@@ -35,6 +40,39 @@
             key = "nebula/host-crt";
           };
         };
+
+        services.nebula.networks.backplane = {
+          enable = true;
+
+          ca = config.sops.secrets."nebula-ca-crt".path;
+          cert = config.sops.secrets."nebula-crt".path;
+          key = config.sops.secrets."nebula-key".path;
+
+          isLighthouse = false;
+          isRelay = false;
+
+          lighthouses = [ ];
+          relays = [ ];
+
+          listen.port = cfg.nebula.port;
+
+          firewall.inbound = lib.mkDefault [
+            {
+              port = "any";
+              proto = "any";
+              host = "any";
+            }
+          ];
+          firewall.outbound = lib.mkDefault [
+            {
+              port = "any";
+              proto = "any";
+              host = "any";
+            }
+          ];
+        };
+
+        networking.firewall.allowedUDPPorts = [ cfg.nebula.port ];
 
         environment.systemPackages = lib.mkIf cfg.nebula.canSign [
           (pkgs.writeShellScriptBin "nebula-sign-host" ''
