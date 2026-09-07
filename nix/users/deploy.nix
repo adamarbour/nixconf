17880a45737f2a5347fbdbe432648dd54aae50ce
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-09-07 00:44:47 -0500

    added my pub key to deploy

--- nix/users/deploy.nix
+++ nix/users/deploy.nix
@@ -7,6 +7,9 @@
       extraGroups = [ "ssh-login" ];
       shell = pkgs.bashInteractive;
       hashedPassword = "!";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEQ3G00lHh4rexwd+spiN9gYwm9iqUn6NWXmjyHRdQT4"
+      ];
     };
   };
 }
