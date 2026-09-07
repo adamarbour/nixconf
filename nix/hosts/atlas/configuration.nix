c530d84e3f8125e291e70d6280ec2ea243851152
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-09-07 12:32:19 -0500

    Fix atlast networking

--- nix/hosts/atlas/configuration.nix
+++ nix/hosts/atlas/configuration.nix
@@ -11,6 +11,16 @@ in
       disko
       self.diskoConfigurations.atlas
     ];
+
+    # network
+    systemd.network.networks."10-wan" = {
+      matchConfig.MACAddress = "00:16:3c:ed:3b:50";
+      networkConfig = {
+        Address = "172.245.210.47/24";
+        Gateway = "172.245.210.1";
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
   };
 
   flake.nixosConfigurations = mkNixos "x86_64-linux" "atlas";
