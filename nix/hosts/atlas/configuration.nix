4e20aee745cdd530bb0f48f9a246de5cbfffbdd0
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-09-07 00:58:28 -0500

    Fix atlas config

--- nix/hosts/atlas/configuration.nix
+++ nix/hosts/atlas/configuration.nix
@@ -3,31 +3,15 @@ let
   inherit (self.lib) mkNixos;
 in
 {
-  flake.modules.nixos.hyphasis = { pkgs, ... }: {
+  flake.modules.nixos.atlas = {
     imports = with self.modules.nixos; [
       # profiles / roles
-      laptop
-      # hardware
-      hw-cpu-intel
-      hw-gpu-intel
-      hw-gpu-nvidia-prime
+      server
       # disk
       disko
-      self.diskoConfigurations.hyphasis
-      # features
-      has-wifi
+      self.diskoConfigurations.atlas
     ];
-
-    boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-
-    services = {
-      xserver.enable = true;
-      displayManager.sddm.enable = true;
-      desktopManager.plasma6.enable = true;
-    };
-
-    programs.firefox.enable = true;
   };
 
-  flake.nixosConfigurations = mkNixos "x86_64-linux" "hyphasis";
+  flake.nixosConfigurations = mkNixos "x86_64-linux" "atlas";
 }
