7c29e77d0860748b5b38a52a91404391161affac
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2026-09-08 00:15:21 -0500

    Fix server kernel

--- nix/modules/profiles/server.nix
+++ nix/modules/profiles/server.nix
@@ -1,6 +1,12 @@
 { self, ... }:
 {
+<<<<<<< HEAD
   flake.modules.nixos.server = { lib, ... }: {
+||||||| parent of ca25adc (Fix server kernel)
+  flake.modules.nixos.server = {
+=======
+  flake.modules.nixos.server = { pkgs, ... }: {
+>>>>>>> ca25adc (Fix server kernel)
     imports = with self.modules.nixos; [
       grub-boot
       networkd
